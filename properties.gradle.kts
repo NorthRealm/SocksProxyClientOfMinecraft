f2056a4267fb733919de740fd13048180353e4e2
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-02-25 14:54:34 +0800

    Use Kotlin for Gradle

---
+++ properties.gradle.kts
@@ -0,0 +1,6 @@
+extra["netty_version"] = "4.1.115.Final"
+extra["dnsjava_version"] = "3.5.2"
+extra["lombok_version"] = "1.18.30"
+extra["compilation_java_version"] = 21
+extra["git_hash"] = "local"
+extra["git_short_hash"] = "local"
