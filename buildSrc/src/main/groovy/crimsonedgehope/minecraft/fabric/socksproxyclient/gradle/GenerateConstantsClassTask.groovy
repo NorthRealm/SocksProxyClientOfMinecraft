af1faae6291d2ceee9ded190c858fcc6fdedde57
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-03-04 18:05:51 +0800

    Gradle plugin

--- buildSrc/src/main/groovy/crimsonedgehope/minecraft/fabric/socksproxyclient/gradle/GenerateConstantsClassTask.groovy
+++ buildSrc/src/main/groovy/crimsonedgehope/minecraft/fabric/socksproxyclient/gradle/GenerateConstantsClassTask.groovy
@@ -1,45 +1,35 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.gradle
 
-import com.google.gson.Gson
-import com.google.gson.JsonObject
-import com.google.gson.stream.JsonReader
 import com.squareup.javapoet.*
 import lombok.NoArgsConstructor
-import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 import javax.lang.model.element.Modifier
 
-abstract class GenerateTranslateKeysTask extends GenerateSourceTask {
-    @InputFile File inputFile
+abstract class GenerateConstantsClassTask extends GenerateSourceTask {
+    @Input Map<String, Object> constants
 
-    GenerateTranslateKeysTask() {
-        super("Generate translate keys. All keys in accordance with en_us.json")
+    GenerateConstantsClassTask() {
+        super("Generate Constants class")
     }
 
     @Override
     @TaskAction
     void generate() throws Exception {
-        final FileReader reader = new FileReader(inputFile)
-        final Gson gson = new Gson()
-        final JsonObject object = gson.fromJson(new JsonReader(reader), JsonObject.class)
-
         final AnnotationSpec lombok =
                 AnnotationSpec.builder(NoArgsConstructor.class)
                         .addMember("access", CodeBlock.builder().add('$L', "lombok.AccessLevel.PRIVATE").build()).build()
 
         final TypeSpec.Builder typeSpec =
-                TypeSpec.classBuilder("TranslateKeys").addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                TypeSpec.classBuilder("Constants").addModifiers(Modifier.PUBLIC, Modifier.FINAL)
                         .addAnnotation(lombok)
 
-        for (String key : object.keySet()) {
-            if (!key.startsWith("socksproxyclient")) {
-                continue
-            }
-            final String k0 = key.toUpperCase().replace(".", "_")
+        for (Map.Entry<String, Object> entry : constants) {
+            final String k0 = entry.getKey().toUpperCase().replaceAll("[\\.\\-]+", "_")
             typeSpec.addField(FieldSpec.builder(String.class, k0)
                     .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
-                    .initializer("\"" + key + "\"").build())
+                    .initializer("\"" + entry.getValue().toString() + "\"").build())
         }
 
         JavaFile out
