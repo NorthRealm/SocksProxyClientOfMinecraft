9938ad5ee4b2c5281ae748a738aacc2717f697e7
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-03-07 23:52:54 +0800

    Refactor

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/GeneralCategory.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/GeneralCategory.java
@@ -1,14 +1,18 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.config.yacl;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.ConfigUtils;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.GeneralConfig;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.config.SocksProxyClientConfig;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry.ProxyEntry;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry.SocksProxyClientConfigEntry;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.yacl.controller.ProxyEntryControllerBuilder;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.yacl.controller.ValidStringControllerBuilder;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.yacl.screen.ProxyEntryEditScreen;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.i18n.TranslateKeys;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.socks.SocksUtils;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.http.HttpProxyUtils;
 import dev.isxander.yacl3.api.ButtonOption;
 import dev.isxander.yacl3.api.ConfigCategory;
 import dev.isxander.yacl3.api.ListOption;
@@ -19,9 +23,27 @@ import dev.isxander.yacl3.api.OptionGroup;
 import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
 import dev.isxander.yacl3.api.controller.StringControllerBuilder;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.MultiplayerServerListPinger;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.toast.SystemToast;
 import net.minecraft.text.Text;
+import net.minecraft.util.Pair;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.Proxy;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 final class GeneralCategory extends YACLCategory<GeneralConfig> {
 
@@ -74,7 +96,7 @@ final class GeneralCategory extends YACLCategory<GeneralConfig> {
                 .name(Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_PROXY_TEST))
                 .description(OptionDescription.of(Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_PROXY_TEST_TOOLTIP)))
                 .available(true)
-                .action((screen, opt) -> SocksUtils.test())
+                .action((screen, opt) -> testProxy())
                 .available(useProxy.getValue())
                 .build();
 
@@ -117,5 +139,134 @@ final class GeneralCategory extends YACLCategory<GeneralConfig> {
         categoryBuilder.group(yaclMinecraftTestSubjects);
 
         return categoryBuilder.build();
+    }
+
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
+    private static final MultiplayerServerListPinger pinger = new MultiplayerServerListPinger();
+    private static Long testTime = 0L;
+
+    static {
+        scheduler.scheduleAtFixedRate(pinger::tick, 0, 50L, TimeUnit.MILLISECONDS);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            scheduler.shutdownNow();
+            pinger.cancel();
+        }));
+    }
+
+    private static void testProxy() {
+        if (System.currentTimeMillis() - testTime <= 5000L) {
+            SocksProxyClientConfig.LOGGER.warn("NO TEST SPAMMING");
+            return;
+        }
+        testTime = System.currentTimeMillis();
+        for (String url : GeneralConfig.getHTTPTestSubjects()) {
+            scheduler.submit(() -> {
+                testHTTP(url);
+            });
+        }
+        for (String domain : GeneralConfig.getMinecraftTestSubjects()) {
+            scheduler.submit(() -> {
+                testMinecraftPing(domain);
+            });
+        }
+    }
+
+    private static void testMinecraftPing(final String target) {
+        try {
+            showTestStart(target);
+            ServerInfo entry = new ServerInfo(target, target, ServerInfo.ServerType.OTHER);
+            pinger.add(entry, () -> {}, () -> {
+                showTestResult(new Pair<>(true, null), target);
+                SocksProxyClientConfig.LOGGER.info("Pinged {}: Ping {}ms\n Version: {}\n Protocol version: {}\n Player count: {}",
+                        target, entry.ping, entry.version.getLiteralString(), entry.protocolVersion, entry.playerCountLabel.getString());
+            });
+        } catch (Exception e) {
+            showTestResult(new Pair<>(false, new RuntimeException("Failed to ping!", e)), target);
+        }
+    }
+
+    private static void testHTTP(final String target) {
+        final CompletableFuture<Pair<Boolean, Throwable>> test = CompletableFuture.supplyAsync(() -> {
+            try {
+                URL url = URI.create(target).toURL();
+                final Proxy httpProxy = HttpProxyUtils.getProxyObject(true);
+                if (httpProxy.equals(Proxy.NO_PROXY)) {
+                    SocksProxyClientConfig.LOGGER.warn("No proxy to test.");
+                    return new Pair<>(true, null);
+                }
+
+                final HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(httpProxy);
+                urlConnection.setConnectTimeout(com.mojang.authlib.minecraft.client.MinecraftClient.CONNECT_TIMEOUT_MS);
+                urlConnection.setReadTimeout(com.mojang.authlib.minecraft.client.MinecraftClient.READ_TIMEOUT_MS);
+                showTestStart(target);
+                int res = urlConnection.getResponseCode();
+
+                if (res != -1) {
+                    if (res == HttpStatus.SC_OK || res == HttpStatus.SC_NO_CONTENT) {
+                        SocksProxyClientConfig.LOGGER.info("{} responded with {}", target, res);
+                    } else {
+                        SocksProxyClientConfig.LOGGER.warn("{} responded with {}", target, res);
+                    }
+                    if (res == HttpStatus.SC_OK) {
+                        final InputStream inputStream = urlConnection.getInputStream();
+                        final String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+                        final Gson gson = new Gson();
+                        final JsonObject jsonObject = gson.fromJson(result, JsonObject.class);
+                        SocksProxyClientConfig.LOGGER.info("{} response: {}", target, jsonObject.toString());
+                    }
+
+                    urlConnection.disconnect();
+                } else {
+                    SocksProxyClientConfig.LOGGER.warn("{} is not responding.", target);
+                }
+            } catch (JsonSyntaxException e) {
+                return new Pair<>(true, new RuntimeException(target + " sent back no json.", e));
+            } catch (IOException e) {
+                return new Pair<>(false, new RuntimeException("IO failure!!", e));
+            }
+            return new Pair<>(true, null);
+        });
+        showTestResult(test, target);
+    }
+
+    private static void showTestStart(final String target) {
+        SocksProxyClientConfig.LOGGER.info("Testing connection to {}", target);
+        MinecraftClient.getInstance().submit(() -> {
+            SystemToast.show(
+                    MinecraftClient.getInstance().getToastManager(),
+                    new SystemToast.Type(1000L),
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_PROXY_TESTING),
+                    Text.literal(target));
+        });
+    }
+
+    private static void showTestResult(Pair<Boolean, Throwable> res, final String target) {
+        MinecraftClient.getInstance().submit(() -> {
+            SystemToast.add(
+                    MinecraftClient.getInstance().getToastManager(),
+                    new SystemToast.Type(),
+                    Text.translatable(res.getLeft()
+                            ? TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_PROXY_TEST_SUCCESS
+                            : TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_PROXY_TEST_FAILURE
+                    ),
+                    Text.literal(target));
+        });
+
+        if (res.getLeft()) {
+            return;
+        }
+
+        Throwable t = res.getRight();
+        SocksProxyClientConfig.LOGGER.error("", t);
+        if (Objects.nonNull(t) && !(t instanceof JsonSyntaxException)) {
+            SocksProxyClientConfig.LOGGER.error("Test not successful.", t);
+        }
+    }
+
+    private static void showTestResult(final CompletableFuture<Pair<Boolean, Throwable>> test, final String target) {
+        test.thenApplyAsync(v -> {
+            showTestResult(v, target);
+            return null;
+        });
     }
 }
--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/proxy/socks/SocksSelection.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/proxy/socks/SocksSelection.java
@@ -4,14 +4,11 @@ import crimsonedgehope.minecraft.fabric.socksproxyclient.SocksProxyClient;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.GeneralConfig;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.ServerConfig;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry.ProxyEntry;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.injection.access.IMixinClientConnection;
 import io.netty.channel.ChannelPipeline;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.network.ServerInfo;
-import net.minecraft.network.ClientConnection;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
@@ -27,44 +24,9 @@ import java.util.function.Supplier;
 public final class SocksSelection {
     private static final Logger LOGGER = SocksProxyClient.getLogger("Connect");
 
-    public static void info(@NotNull List<ProxyEntry> proxies, @NotNull InetSocketAddress remote) {
-        Objects.requireNonNull(remote.getHostString());
-        if (proxies.isEmpty()) {
-            LOGGER.info("[Direct] -> [Remote] {}:{}", remote.getHostString(), remote.getPort());
-            return;
-        }
-        StringBuilder builder = new StringBuilder();
-        for (ProxyEntry entry : proxies) {
-            builder.append(String.format("[%s] %s:%s -> ",
-                    entry.getVersion().description,
-                    ((InetSocketAddress) entry.getProxy().address()).getHostString(),
-                    ((InetSocketAddress) entry.getProxy().address()).getPort()));
-        }
-        builder.append(String.format("[Remote] %s:%s", remote.getHostString(), remote.getPort()));
-        LOGGER.info("{}", builder);
-    }
-
     public static final Function<InetAddress, Supplier<List<ProxyEntry>>> supplierForMinecraft = (address) -> ServerConfig::getProxyEntryForMinecraft;
 
     public static final Supplier<List<ProxyEntry>> supplier = () -> GeneralConfig.getProxyEntry(true);
-
-    public static void fire(@NotNull ClientConnection clientConnection, @NotNull ChannelPipeline pipeline) {
-        IMixinClientConnection imixin = (IMixinClientConnection) clientConnection;
-
-        ServerInfo serverInfo = imixin.socksProxyClient$getServerInfo();
-        InetSocketAddress remote = imixin.socksProxyClient$getInetSocketAddress();
-
-        // TODO: Remove
-        if (Objects.nonNull(serverInfo) && serverInfo.getServerType().equals(ServerInfo.ServerType.OTHER)) {
-            if (serverInfo.address.equals("mc.hypixel.net")) {
-                apply(remote, List.of(), pipeline);
-                return;
-            }
-        }
-
-        InetAddress address = remote.getAddress();
-        fire(remote, pipeline, supplierForMinecraft.apply(address));
-    }
 
     public static void fire(InetSocketAddress remote, ChannelPipeline pipeline) {
         fire(remote, pipeline, supplier);
@@ -81,5 +43,22 @@ public final class SocksSelection {
     private static void apply(@NotNull InetSocketAddress remote, @NotNull List<ProxyEntry> proxies, @NotNull ChannelPipeline pipeline) {
         SocksUtils.apply(pipeline, proxies);
         info(proxies, remote);
+    }
+
+    private static void info(@NotNull List<ProxyEntry> proxies, @NotNull InetSocketAddress remote) {
+        Objects.requireNonNull(remote.getHostString());
+        if (proxies.isEmpty()) {
+            LOGGER.info("[Direct] -> [Remote] {}:{}", remote.getHostString(), remote.getPort());
+            return;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (ProxyEntry entry : proxies) {
+            builder.append(String.format("[%s] %s:%s -> ",
+                    entry.getVersion().description,
+                    ((InetSocketAddress) entry.getProxy().address()).getHostString(),
+                    ((InetSocketAddress) entry.getProxy().address()).getPort()));
+        }
+        builder.append(String.format("[Remote] %s:%s", remote.getHostString(), remote.getPort()));
+        LOGGER.info("{}", builder);
     }
 }
