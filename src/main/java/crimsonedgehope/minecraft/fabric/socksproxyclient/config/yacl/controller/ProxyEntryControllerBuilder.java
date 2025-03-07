2083f3c93a68c791f2fbfed76c28791b23674ba1
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-03-07 23:52:33 +0800

    Refactor

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/controller/ProxyEntryControllerBuilder.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/controller/ProxyEntryControllerBuilder.java
@@ -4,8 +4,11 @@ import crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry.ProxyEntry
 import dev.isxander.yacl3.api.Option;
 import dev.isxander.yacl3.api.controller.ControllerBuilder;
 import dev.isxander.yacl3.gui.YACLScreen;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import org.apache.logging.log4j.util.TriConsumer;
 
+@Environment(EnvType.CLIENT)
 public interface ProxyEntryControllerBuilder extends ControllerBuilder<ProxyEntry> {
     static ProxyEntryControllerBuilderImpl create(Option<ProxyEntry> option) {
         return new ProxyEntryControllerBuilderImpl(option);
