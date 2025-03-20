2ae2b2409edb0046356f0418b7c53ab863fb19ee
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-03-20 23:25:26 +0800

    patch

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/access/IMixinMultiValueDebugSampleLogImpl.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/access/IMixinMultiValueDebugSampleLogImpl.java
@@ -1,5 +1,9 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.injection.access;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
 public interface IMixinMultiValueDebugSampleLogImpl {
     boolean socksProxyClient$isPingingUseProxy();
     void socksProxyClient$setPingingUseProxy(boolean useProxy);
