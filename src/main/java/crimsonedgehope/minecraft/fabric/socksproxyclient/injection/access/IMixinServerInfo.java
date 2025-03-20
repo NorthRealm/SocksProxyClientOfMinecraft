897a865adc41bc363bd5a3c10746c7a197d17b60
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-03-20 23:25:26 +0800

    patch

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/access/IMixinServerInfo.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/access/IMixinServerInfo.java
@@ -1,5 +1,9 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.injection.access;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
 public interface IMixinServerInfo {
     boolean socksProxyClient$isUseProxy();
     void socksProxyClient$setUseProxy(boolean useProxy);
